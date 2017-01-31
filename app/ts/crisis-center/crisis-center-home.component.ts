0f5b2672ce24e25d9386173bf711e4881d8d1f60
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-01 00:38:32 +0530

    Developed crisis-center-home.component. 2. Developed CSS for crisis-center-home.component.css and heroes-list.component.html

---
+++ app/ts/crisis-center/crisis-center-home.component.ts
@@ -0,0 +1,9 @@
+import {
+    Component
+} from '@angular/core';
+
+@Component({
+    templateUrl: "app/ts/crisis-center/crisis-center-home.component.html",
+    styleUrls: ["app/ts/crisis-center/crisis-center-home.component.css"]
+})
+export class CrisisCenterHomeComponent {}
