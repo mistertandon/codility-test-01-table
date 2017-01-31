5d282966b4359ad3abb3c9a2b39fb5415ec9b5e2
Mister Tandon <enggparveshtandon@gmail.com>
2017-01-31 23:27:35 +0530

    Developed code to render crisis list using crisis-list.component. 2. Styled crisis-list.component view page.

--- app/ts/crisis-center/crisis-center.component.ts
+++ app/ts/crisis-center/crisis-center.component.ts
@@ -1,44 +1,12 @@
 import {
-    Component,
-    OnInit
+    Component
 } from '@angular/core';
-
-import {
-    Observable
-} from 'rxjs/Observable';
-
-import {
-    CrisisCenterClass
-} from './../custom-classes/crisis-center.class';
-
-import {
-    CrisisCenterService
-} from './crisis-center.service';
 
 @Component({
     templateUrl: "app/ts/crisis-center/crisis-center.component.html",
     styleUrls: ["app/ts/crisis-center/crisis-center.component.css"]
 })
-export class CrisisCenterComponent implements OnInit {
+export class CrisisCenterComponent {
 		
-		/**
-		 * crisisList: Used to store crisis information array.
-		 *
-		 **/
-    public crisisList: CrisisCenterClass[];
-		
-    constructor(private CrisisCenterService_S: CrisisCenterService) {
 
-    }
-
-    ngOnInit() {
-
-        this.CrisisCenterService_S.getCrisisCenterList()
-            .then(
-                (crisisInfoArr) => {
-
-                    this.crisisList = crisisInfoArr;
-                });
-
-    }
 }
--- app/ts/crisis-center/crisis-list.component.ts
+++ app/ts/crisis-center/crisis-list.component.ts
@@ -1,14 +1,42 @@
-import {Component} from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+
+import {
+    CrisisCenterClass
+} from './../custom-classes/crisis-center.class';
+
+import {
+    CrisisCenterService
+} from './crisis-center.service';
 
 @Component({
-templateUrl: "app/ts/crisis-center/crisis-list.component.html",
-styleUrls: [ "app/ts/crisis-center/crisis-list.component.css"],
+    templateUrl: "app/ts/crisis-center/crisis-list.component.html",
+    styleUrls: ["app/ts/crisis-center/crisis-list.component.css"],
 
 })
-export class CrisisListComponent{
-	
+export class CrisisListComponent implements OnInit {
 
-	public constructor(){
-	
-	}
-} 
+    /**
+     * crisisList: Used to store crisis information array.
+     *
+     **/
+    public crisisList: CrisisCenterClass[];
+
+    constructor(private CrisisCenterService_S: CrisisCenterService) {
+
+    }
+
+    ngOnInit() {
+
+        this.CrisisCenterService_S.getCrisisCenterList()
+            .then(
+                (crisisInfoArr) => {
+
+                    this.crisisList = crisisInfoArr;
+                });
+
+    }
+
+}
