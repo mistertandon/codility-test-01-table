c99821cc7680c99e5e823f177c2ef29bea81d400
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-02 12:16:18 +0530

    Create file and setup initial routing to get started with posts module.

---
+++ app/ts/post-module/post.component.ts
@@ -0,0 +1,9 @@
+import {
+    Component
+} from '@angular/core';
+
+@Component({
+    templateUrl: "app/ts/post-module/post.component.html",
+    styleUrls: ["app/ts/post-module/post.component.css"]
+})
+export class PostComponent {}
