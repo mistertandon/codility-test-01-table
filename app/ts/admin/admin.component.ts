e1005b54c127def9eff253e1ad8f17f1c42cb1c5
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-04 22:10:35 +0530

    Created required file to get started with admin featured area.

---
+++ app/ts/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: "app/ts/admin/admin.component.html",
+  styleUrls: ["app/ts/admin/admin.component.css"]
+})
+export class AdminComponent {
+
+  public constructor() {
+
+  }
+
+}
