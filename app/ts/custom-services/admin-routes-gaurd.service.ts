fdd5c6636ac0ab0ff6b0f5b0ac185cec9b71b23a
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-05 02:13:35 +0530

    Modfied AdminRoutesGaurdService to redirect  user to login component when un-authenticated user trying to access admin page.

--- app/ts/custom-services/admin-routes-gaurd.service.ts
+++ app/ts/custom-services/admin-routes-gaurd.service.ts
@@ -3,21 +3,30 @@ import { Injectable } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
-
+@Injectable()
 export class AdminRoutesGaurdService implements CanActivate {
 
+  constructor(private Router_S: Router) {
+
+  }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
     return this.validateUserLogin();
   }
-  
+
   /**
    * This function is used to validate user login state and return boolean true OR false for
    * corresponding.
    */
   private validateUserLogin(): boolean {
 
-    return false;
+    if (true) {
+
+      this.Router_S.navigate(['login']);
+
+      return true;
+
+    }
   }
 
 }
